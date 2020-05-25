a05479a49a7f63fe1571cb586fcb7b5be1a7a4ce
Jerem <jerem.blcn@gmail.com>
2020-05-25 10:26:26 +0200

    First commit

---
+++ app/src/main/java/com/mds/cocktapp/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mds.cocktapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mds.cocktapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
